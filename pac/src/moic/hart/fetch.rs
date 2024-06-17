defc706a10e2494ed6a2de61f96201e6b57f6bc1
zflcs <1491657576@qq.com>
2024-06-17 11:46:01 +0800

    Update pac specification.

--- pac/src/moic/hart/fetch.rs
+++ pac/src/moic/hart/fetch.rs
@@ -1,13 +1,12 @@
 #[doc = "Register `fetch` reader"]
 pub type R = crate::R<FetchSpec>;
-impl core::fmt::Debug for R {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        write!(f, "{}", self.bits())
-    }
-}
-impl core::fmt::Debug for crate::generic::Reg<FetchSpec> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        core::fmt::Debug::fmt(&self.read(), f)
+#[doc = "Field `tcb` reader - The pointer of task control block."]
+pub type TcbR = crate::FieldReader<u64>;
+impl R {
+    #[doc = "Bits 6:64 - The pointer of task control block."]
+    #[inline(always)]
+    pub fn tcb(&self) -> TcbR {
+        TcbR::new((self.bits >> 6) & 0x07ff_ffff_ffff_ffff)
     }
 }
 #[doc = "Fetch a task from the priority queue.\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`fetch::R`](R).  See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
